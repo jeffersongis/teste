4119532d9009df0744cfce4cf7c50977ed8d60a1
jeffersongis <55758046+jeffersongis@users.noreply.github.com>
2020-07-11 18:23:38 -0400

    D
    D

---
+++ sinop/styles/sede_navegacao_15_style.js
@@ -0,0 +1,42 @@
+var size = 0;
+var placement = 'point';
+
+var style_sede_navegacao_15 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    var clusteredFeatures = feature.get("features");
+    var labelFont = "13.0px \'Calisto MT\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "#ffffff";
+    var bufferWidth = 1.0;
+    size = clusteredFeatures.length;
+    var textAlign = "center";
+    var offsetX = 0;
+    var offsetY = 0;
+    if (size == 1) {
+        textAlign = "left"
+        offsetX = 8
+        offsetY = 3
+        var feature = clusteredFeatures[0];
+        if (feature.get("Codigo") !== null) {
+            labelText = String(feature.get("Codigo"));
+        }
+        key = value + "_" + labelText
+    } else {
+        labelText = size.toString()
+        size = 2*(Math.log(size)/ Math.log(2))
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 8.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1}), fill: new ol.style.Fill({color: 'rgba(233,222,223,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
