108864bc80944f7e02401a01149c03de8f4d45ad
jeffersongis <jeffersongis@hotmail.com>
2022-02-14 19:13:45 -0400

    site cprverde
    versão 1.0

---
+++ cprverde/js/006-rodape.js
@@ -0,0 +1,5 @@
+document.getElementById("rodape").innerHTML=
+    '<p class="texto-corrido-bold">Relatório online de CPR Verde</p>'+
+    '<p class="texto-pequeno-2">Todos os direitos reservados<br>'+
+    'INOVASIG LTDA<br>'+
+    '</p>'
