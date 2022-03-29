d234c8812efea9b0668f35f463eeade660fe9893
jeffersongis <jeffersongis@hotmail.com>
2022-03-29 15:19:34 -0400

    Mapa hidro

--- mapadocar/js/002-navegaMenu.js
+++
@@ -1,8 +0,0 @@
-function getPos(id) {//parametro é a seção
-    let el = document.getElementById(id);//obtém a seção
-    for (var lx=0, ly=0;
-        el != null;
-        lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);
-        ly = ly-96;//posicao da secao menos a altura do menu nav
-        window.scrollTo(0, ly);//vai até a altura da seção na página;
-}
