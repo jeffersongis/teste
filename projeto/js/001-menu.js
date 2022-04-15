18924b21b88ba2a71a4243e6291c93bf334a37d9
jeffersongis <jeffersongis@hotmail.com>
2022-04-15 10:26:45 -0400

    Projeto
    projeto

---
+++ projeto/js/001-menu.js
@@ -0,0 +1,24 @@
+document.getElementById("navbar").innerHTML=
+    '<a href="index.html" class="logo_menu"><img src="img/001-LOGO.png" class="logo"></a>'+
+                '<ul class="nav-menu">'+
+                    '<li class="nav-item">'+
+                        '<a href="#banner" class="nav-link">Início</a>'+
+                    '</li>'+
+                    '<li class="nav-item">'+
+                        '<p id="link1" class="nav-link" onclick="getPos(\'ativoambiental\')">Ativo Ambiental</p><!--usei js para que a barra de navegação não cobrisse o titulo da seção-->'+
+                    '</li>'+
+                    '<li class="nav-item">'+
+                        '<p id="link2" class="nav-link" onclick="getPos(\'cprverde\')">CPR Verde</p><!--usei js para que a barra de navegação não cobrisse o titulo da seção-->' +
+                    '</li>' +
+                    '<li class="nav-item">' +
+                        '<p id="link3" class="nav-link" onclick="getPos(\'comprovacao\')">Comprovações</p><!--usei js para que a barra de navegação não cobrisse o titulo da seção-->' +
+                    '</li>' +
+                    '<li class="nav-item">' +
+                '</ul>' +
+                '<a href="https://jeffersongis.github.io/teste/portalcpr/index.html" target="_blank" class="botao_entrar"><button class="botao_primario botao_resp">Administração</button></a>' +
+
+                '<div class="hamburger">' +
+                    '<span class="bar"></span>' +
+                    '<span class="bar"></span>' +
+                    '<span class="bar"></span>' +
+                '</div>'
---
+++ projeto/js/002-navegaMenu.js
@@ -0,0 +1,8 @@
+function getPos(id) {//parametro é a seção
+    let el = document.getElementById(id);//obtém a seção
+    for (var lx=0, ly=0;
+        el != null;
+        lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);
+        ly = ly-40;//posicao da secao menos a altura do menu nav
+        window.scrollTo(0, ly);//vai até a altura da seção na página;
+}
