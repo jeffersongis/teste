ebd2f3e3480d4104c92262c4d61eccfd2b1a47c8
jeffersongis <jeffersongis@hotmail.com>
2025-03-13 16:44:47 -0400

    deletei projetos antigos

--- cprverde1/js/006-rodape.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("rodape").innerHTML=
-    '<p class="texto-corrido-bold">Relatório online de CPR Verde</p>'+
-    '<p class="texto-pequeno-2">Todos os direitos reservados<br>'+
-    'INOVASIG LTDA<br>'+
-    '</p>'
--- portalcprativo2/js/006-rodape.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("rodape").innerHTML=
-    '<p class="texto-corrido-bold">Instituto Ação Verde</p>'+
-    '<p class="texto-pequeno-2">Avenida Historiador Rubens de Mendonça, 4.193<br>'+
-    'Centro Político Administrativo - Condomínio Casa da Indústria<br>'+
-    'CEP 78050-000 - Cuiabá-MT<br>'+
-    'telefone: (65) 3611-1606<br>'+
-    'email: acaverdefinanceiro@gmail.com'+
-    '</p>'
